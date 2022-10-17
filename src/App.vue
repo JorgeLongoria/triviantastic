3fe24ae1bd878d98859407e831115b1123bc2ed7
Jorge Longoria <jlongo48@stu.southtexascollege.edu>
2022-10-17 15:56:18 -0500

    Initial cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
-<script setup>
-  import NavMenu from '@/components/NavMenu.vue'
-</script>
+<script setup></script>
 
 <template>
   <NavMenu />
--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -1,5 +1,3 @@
-<script setup></script>
-
 <template>
   <h1 class="text-6xl font-thin text-slate-800">I'm Question Page</h1>
 </template>
